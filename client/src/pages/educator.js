3cbfdf2905d31c6d927da546bfcf5d893cec33ee
Aaron Sims <aaron@adsimsllc.com>
2020-10-09 16:22:20 -0400

    updating app.js file with necessary pages

---
+++ client/src/pages/educator.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Educator = (props) => {
+  return <div>welcome to curated syllabus content</div>;
+};
+
+Educator.propTypes = {};
+
+export default Educator;
---
+++ client/src/pages/researcher.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Researcher = (props) => {
+  return <div>Thank you for submitting your Research!</div>;
+};
+
+Researcher.propTypes = {};
+
+export default Researcher;
