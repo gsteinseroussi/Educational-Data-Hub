74892a5f3befedcffe9e4c0426b1b6e12b956db1
Aaron Sims <aaron@adsimsllc.com>
2020-10-08 21:00:01 -0400

    creating app.js file

--- educational-data-hub/src/App.js
+++ educational-data-hub/src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+// import "./pages/Home";
+// import "./pages/Login";
+// import "./pages/Researcher";
+// import "./pages/Editor";
+// import "./pages/Educator";
+// import "./pages/NoMatch";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <Router>
+    //   <div>
+    //     <Nav />
+    //     <Switch>
+    //       <Route exact path="/" component={Home} />
+    //       <Route exact path="/home" component={Home} />
+    //       <Route exact path="/login" component={Login} />
+    //       <Route exact path="/researcher" component={Researcher} />
+    //       <Route exact path="/editor" component={Editor} />
+    //       <Route exact path="/educator" component={Educator} />
+    //       <Route exact path="*" component={NoMatch} />
+    //     </Switch>
+    //   </div>
+    // </Router>
   );
 }
 
