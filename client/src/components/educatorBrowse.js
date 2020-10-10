3ae5569c05ca0e1db04e2aae6e3c0f97d342f181
Garrett Stein-Seroussi <gsteinseroussi@gmail.com>
2020-10-10 13:22:16 -0400

    removed node_modules

--- client/src/components/educatorBrowse.js
+++ client/src/components/educatorBrowse.js
@@ -21,16 +21,7 @@ const EducatorBrowse = () => {
   <label className="form-check-label" htmlFor="inlineCheckbox2">9-12</label>
 </div>
 
-<div className="dropdown">
-  <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-    Dropdown
-  </button>
-  <div className="dropdown-menu" aria-labelledby="dropdownMenu2">
-    <button className="dropdown-item" type="button">Action</button>
-    <button className="dropdown-item" type="button">Another action</button>
-    <button className="dropdown-item" type="button">Something else here</button>
-  </div>
-</div>
+
         </div>
       </div>
     </div>
