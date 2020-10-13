c045a8ea93fc2584a169a902ee9336ac879164db
paulsloderbeck <64672063+paulsloderbeck@users.noreply.github.com>
2020-10-13 19:24:02 -0400

    added articlesController

---
+++ controllers/articlesController.js
@@ -0,0 +1,35 @@
+const db = require("../database/models");
+
+//defining methods for articlesController
+module.exports = {
+  findAll: function (req, res) {
+    db.Article.find(req.query)
+      .sort({ date: -1 })
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  findById: function (req, res) {
+    db.Article.findById(req.params.id)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  create: function (req, res) {
+    db.Article.create(req.body)
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  },
+  update: function (req, res) {
+    db.Article.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body
+    ).then((dbModel) =>
+      res.json(dbModel).catch((err) => res.status(422).json(err))
+    );
+  },
+  remove: function (req, res) {
+    db.Article.findById({ _id: req.params.id })
+      .then((dbModel) => dbModel.remove())
+      .then((dbModel) => res.json(dbModel))
+      .catch((err) => res.status(422).json(err));
+  }
+};
