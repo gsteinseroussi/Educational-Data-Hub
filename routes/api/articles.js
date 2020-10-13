aeb475312155b61bea0135ee74d83f23c9936880
paulsloderbeck <64672063+paulsloderbeck@users.noreply.github.com>
2020-10-13 19:40:44 -0400

    added article api routes

---
+++ routes/api/articles.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const articlesController = require("../../controllers/articlesController");
+
+//Matches with "/api/articles"
+router
+  .route("/")
+  .get(articlesController.findAll)
+  .post(articlesController.create);
+
+//Matches with "/api/articles/:id"
+router
+  .route("/:id")
+  .get(articlesController.findById)
+  .put(articlesController.update)
+  .delete(articlesController.remove);
+
+module.exports = router;
