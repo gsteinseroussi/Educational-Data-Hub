319caeceec07e24cf57b6b35d3da25e5ad0e1ca6
Garrett Stein-Seroussi <gsteinseroussi@gmail.com>
2020-10-20 21:51:56 -0400

    deploying

--- database/index.js
+++ database/index.js
@@ -8,7 +8,8 @@ mongoose
   .connect(process.env.MONGO_URI || "mongodb://localhost/tempDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    useCreateIndex: true
+    useCreateIndex: true,
+    useFindandModify: false
   })
   .then(
     () => {
