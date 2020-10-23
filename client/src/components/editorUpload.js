d23fa822b0a111ca1b6d5f87a4f5e5016a56d064
Lisa Ellison <lisaellison9@gmail.com>
2020-10-23 17:32:56 -0400

    styled editor page

--- client/src/components/editorUpload.js
+++ client/src/components/editorUpload.js
@@ -1,15 +1,22 @@
 import React, { useState, useEffect } from "react";
 import API from "../utils/lessonAPI";
-import GradeSelector from "./gradeSelector"
+import GradeSelector from "./gradeSelector";
 
 function EditorUpload() {
   // set component's initial state
   const [formObject, setFormObject] = useState({});
-  const [gradeChoices, setGradeChoices] = useState({"k-5": false, "6-8": false, "9-12": false})
+  const [gradeChoices, setGradeChoices] = useState({
+    "k-5": false,
+    "6-8": false,
+    "9-12": false,
+  });
 
-  const handleGradeChange = (event)=>{
-    setGradeChoices({...gradeChoices, [event.target.name]: event.target.checked})
-  }
+  const handleGradeChange = (event) => {
+    setGradeChoices({
+      ...gradeChoices,
+      [event.target.name]: event.target.checked,
+    });
+  };
 
   //update component state when the user types into input field
   function handleInputChange(event) {
@@ -21,10 +28,10 @@ function EditorUpload() {
   function handleFormSubmit(event) {
     event.preventDefault();
     // if either of the respective boxes is checked, add selection to array to add to database
-    let gradeLevel=[]
-    if (gradeChoices["k-5"]) gradeLevel.push("k-5")
-    if (gradeChoices["6-8"]) gradeLevel.push("6-8")
-    if (gradeChoices["9-12"]) gradeLevel.push("9-12")
+    let gradeLevel = [];
+    if (gradeChoices["k-5"]) gradeLevel.push("k-5");
+    if (gradeChoices["6-8"]) gradeLevel.push("6-8");
+    if (gradeChoices["9-12"]) gradeLevel.push("9-12");
 
     // checking to make sure that all the input boxes are selected
     if (
@@ -39,69 +46,61 @@ function EditorUpload() {
         lessonName: formObject.lessonName,
         authorName: formObject.authorName,
         lessonAbstract: formObject.lessonAbstract,
-        gradeLevel
+        gradeLevel,
       })
         .then(console.log(formObject))
         .catch((err) => console.log(err));
     }
   }
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col">
-          <div className="jumbotron">
-            <div className="container">
-              <h2>Directions for Editors</h2>
-              <p> Explanations about what to enter</p>
-              <p> Directions for permissions on Google Doc Submission</p>
-            </div>
+    <div className="card">
+      <div className="card-body">
+        <div className="searchContainer">
+          <h2>Upload Form</h2>
+          <div className="form-group">
+            <input
+              className="form-control"
+              onChange={handleInputChange}
+              name="lessonName"
+              placeholder="Lesson Plan Name (required)"
+            ></input>
           </div>
-        </div>
-        <div className="col">
-          <div className="jumbotron">
-            <h2>Upload Form</h2>
-            <div className="form-group">
-              <input
-                className="form-control"
-                onChange={handleInputChange}
-                name="lessonName"
-                placeholder="Lesson Plan Name (required)"
-              ></input>
-            </div>
-            <div className="form-group">
-              <input
-                className="form-control mb-4"
-                onChange={handleInputChange}
-                name="authorName"
-                placeholder="Author Name (required)"
-              ></input>
-            </div>
-            <div className="form-group">
-              <input
-                className="form-control mb-4"
-                onChange={handleInputChange}
-                name="researchDocLink"
-                placeholder="Google Docs Link (required)"
-              ></input>
-            </div>
-            <GradeSelector gradeChoices={gradeChoices} onChange={handleGradeChange}/>
-            <div className="form-group">
-              <textarea
-                className="form-control mb-4"
-                onChange={handleInputChange}
-                name="lessonAbstract"
-                rows="6"
-                placeholder="Abstract (required)"
-              ></textarea>
-            </div>
-            <button
-              type="button"
-              className="btn btn-secondary"
-              onClick={handleFormSubmit}
-            >
-              Submit
-            </button>
+          <div className="form-group">
+            <input
+              className="form-control mb-4"
+              onChange={handleInputChange}
+              name="authorName"
+              placeholder="Author Name (required)"
+            ></input>
           </div>
+          <div className="form-group">
+            <input
+              className="form-control mb-4"
+              onChange={handleInputChange}
+              name="researchDocLink"
+              placeholder="Google Docs Link (required)"
+            ></input>
+          </div>
+          <GradeSelector
+            gradeChoices={gradeChoices}
+            onChange={handleGradeChange}
+          />
+          <div className="form-group">
+            <textarea
+              className="form-control mb-4"
+              onChange={handleInputChange}
+              name="lessonAbstract"
+              rows="6"
+              placeholder="Abstract (required)"
+            ></textarea>
+          </div>
+          <button
+            type="button"
+            className="btn btn-secondary"
+            onClick={handleFormSubmit}
+          >
+            Submit
+          </button>
         </div>
       </div>
     </div>
--- client/src/pages/editor.js
+++ client/src/pages/editor.js
@@ -8,11 +8,35 @@ const Editor = (props) => {
   return (
     <div className="editorPage">
       <div className="jumbotron">
-        <h2 className="display-4">Where data meets implementation</h2>
+        <h1 className="display-4">Educational Data Stream</h1>
+        <h2 className="subhead">Where data meets implementation</h2>
       </div>
       <div className="container-fluid">
         <div className="row">
           <div className="col-md-6">
+            <div className="card">
+              <div className="card-body">
+                <div className="searchContainer">
+                  <h2>How to use this page</h2>
+                  <ol>
+                    <li>
+                      Process the article presented to you upon loading the
+                      editorPage
+                    </li>
+                    <li>
+                      Create lesson plans and place them in a Google document
+                    </li>
+                    <li>
+                      Adjust the "Share" settings so anyone with a link can edit
+                    </li>
+                    <li>
+                      Complete the form & submit the lesson plan link as a
+                      Google document
+                    </li>
+                  </ol>
+                </div>
+              </div>
+            </div>
             <EditorPreview />
           </div>
           <div className="col-md-6">
