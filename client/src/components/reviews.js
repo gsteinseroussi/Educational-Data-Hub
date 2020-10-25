7bc9cc378aea6e625908484295281aa53887c60d
Lisa Ellison <lisaellison9@gmail.com>
2020-10-25 10:38:39 -0400

    attempt to make font dark in reviews list

--- client/src/components/reviews.js
+++ client/src/components/reviews.js
@@ -1,16 +1,13 @@
 import React from "react";
 import Comments from "./comments";
+import "./edBrowseEtc.css";
 
 const Reviews = (props) => {
   return (
     <div className="card">
       <div className="card-body">
-        <div className="searchContainer">
+        <div className="searchContainer darkFont">
           <h2 className="card-title">Lesson Plan Reviews</h2>
-          <p>
-            Read what other educators thought about the lesson plan & add your
-            own thoughts.
-          </p>
           <Comments
             viewDetails={props.viewDetails}
             lessonID={props.lessonID}
